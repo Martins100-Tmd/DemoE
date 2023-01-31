e92ae2a87f5dc5b7a04a8e82b43c1c9de0461bbb
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-01-31 09:21:13 +0100

    Demo express

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const { all } = require("./Obj");
 const path = require("path");
+require("dotenv").config();
 
 app.use(express.static(path.join(__dirname, "public")));
 
