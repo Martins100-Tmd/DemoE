211559c8e83f834d5469bae9bdacad9c8819777c
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-01-31 14:38:02 +0100

    Vercel

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
 const { all } = require("./Obj");
-const path = require("path");
 require("dotenv").config();
 
-app.use(express.static(path.join(__dirname, "public")));
-
 app.get("/", (req, res) => {
-  res.render("Index.js");
+  res.json(all);
 });
 
-app.listen(3000, () => {
-  console.log("listening on PORT 3000");
+app.listen(4000, () => {
+  console.log("listening on PORT 4000");
 });
