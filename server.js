a25fad152174e7fe3ece9b928a0d146fb363658e
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-02-01 17:36:56 +0100

    ...

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ require("dotenv").config();
 app.get("/", (req, res) => {
   res.json(all);
 });
+app.get("/world", (req, res) => {
+  res.status(200).json({ message: "Hello World", success: true });
+});
 
 app.listen(4000, () => {
   console.log("listening on PORT 4000");
