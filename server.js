57fc0ab8ec539c55cbd61fa590e3a3a75f392f28
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-02-02 16:39:43 +0100

    ...

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const { all } = require("./Obj");
 require("dotenv").config();
+const path = require("path");
 
+app.use(express.static(path.join(__dirname, "./public")));
 app.get("/", (req, res) => {
   res.json(all);
 });
@@ -10,6 +12,9 @@ app.get("/world", (req, res) => {
   res.status(200).json({ message: "Hello World", success: true });
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "Index.html"));
+});
 app.listen(4000, () => {
   console.log("listening on PORT 4000");
 });
